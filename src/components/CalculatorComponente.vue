62c171e2c929089276f52c48954e6a9619f215ea
albertbrum <65706837+albertbrum@users.noreply.github.com>
2023-10-18 11:22:59 -0300

    Inclusão de lógica teclado e display calculadora

--- src/components/CalculatorComponente.vue
+++ src/components/CalculatorComponente.vue
@@ -8,14 +8,18 @@
           </q-card-section>
           <q-card-section>
             <div class="text-h5 text-grey-5 text-right">
-              sentença da expressao numerica
+              {{ displayNumberSentence }}
             </div>
             <div class="text-h3 text-right">resultado</div>
           </q-card-section>
 
           <q-card-section class="bg-grey-4">
             <div class="row q-col-gutter-sm">
-              <div class="col-3" v-for="(btn, index) in buttons" :key="index">
+              <div
+                class="col-3"
+                v-for="(btn, index) in buttons"
+                :key="index"
+              >
                 <q-btn
                   class="full-width text-h6"
                   :color="notIsNumber(btn) ? 'indigo' : 'grey-2'"
@@ -26,10 +30,20 @@
                 </q-btn>
               </div>
               <div class="col-6">
-                <q-btn class="full-width text-h6" color="indigo"> CE </q-btn>
+                <q-btn
+                  class="full-width text-h6"
+                  color="indigo"
+                >
+                  CE
+                </q-btn>
               </div>
               <div class="col-6">
-                <q-btn class="full-width text-h6" color="orange"> = </q-btn>
+                <q-btn
+                  class="full-width text-h6"
+                  color="orange"
+                >
+                  =
+                </q-btn>
               </div>
             </div>
           </q-card-section>
@@ -40,31 +54,58 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const buttons: (number | string)[] = [
   7,
   8,
   9,
-  "%",
+  '%',
   4,
   5,
   6,
-  "+",
+  '+',
   1,
   2,
   3,
-  "-",
-  ".",
+  '-',
+  '.',
   0,
-  "/",
-  "*",
+  '/',
+  '*',
 ];
 
-const notIsNumber = (valueNumberOrOperator: number | string): boolean =>
-  isNaN(Number(valueNumberOrOperator));
+type numberOrOperator = number | string;
+type displayNumberSentence = number | string;
+type notIsNumber = number | string;
 
-const btnAction = (btn: number | string) => {
-  if (!notIsNumber(btn)) {
-    console.log("numero");
+const notIsNumber = (value: numberOrOperator): boolean => isNaN(Number(value));
+
+const displayNumberSentence = ref('');
+
+const btnAction = (value: numberOrOperator) => {
+  if (!notIsNumber(value)) {
+    displayNumberSentence.value += String(value);
+  } else {
+    performOperation(value);
+  }
+};
+
+const performOperation = (value: numberOrOperator) => {
+  if (value === '.') {
+    if (value.indexOf('.') === -1) {
+      displayNumberSentence.value += String(value);
+    }
+  } else {
   }
 };
 </script>
+
+<style>
+.text-h5 {
+  height: 23px;
+}
+.text-h3 {
+  height: 50px;
+}
+</style>
